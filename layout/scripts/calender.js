9ed99ebf7a8ffe4d505e62cf2bb79edd2f8207dc
JayawardanaJTH <tiran2323@gmail.com>
2021-02-10 01:09:38 +0530

    correct calender functions

--- layout/scripts/calender.js
+++ layout/scripts/calender.js
@@ -1,51 +1,77 @@
-const date = new Date();
+let date = new Date()
 
-const monthDays = document.querySelector('.day');
-
+let dateGenerate = function(){
+    const year = new Date(date.getFullYear(),date.getMonth()).getFullYear();
+    
     //past month last day
-const lastDay = new Date(date.getFullYear(),date.getMonth(),0).getDate();
-console.log(lastDay);
-//current month first day index
-const firstDayIndex = new Date(date.getFullYear(),date.getMonth());
+    const pmlastDay = new Date(date.getFullYear(),date.getMonth(),0).getDate();
 
-console.log(firstDayIndex);
+    //current month first day index
+    const cmfirstDayIndex = new Date(date.getFullYear(),date.getMonth(),1).getDay();
+    //current month last day index
+    const cmlastDayIndex = new Date(date.getFullYear(),date.getMonth()+1,0).getDay();
+    //current month last day 
+    const cmlastDay = new Date(date.getFullYear(),date.getMonth()+1,0).getDate();
 
-const today = date.getDate();
+    const today = date.getDate();
 
-const curr_mon_first_day_index = new Date(date.getFullYear(),date.getMonth(),1).getDay();
-const curr_mon_last_day_index = date.getDay();
-console.log(curr_mon_first_day_index);
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
 
-const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-];
+    const monthDays = document.querySelector('.day');
 
-document.querySelector('.days h1').innerHTML
-= months[date.getMonth()];
+    document.querySelector('.days h1').innerHTML
+    = months[date.getMonth()];
 
-document.querySelector('.days p').innerHTML
-= date.toDateString();
+    document.querySelector('.days p').innerHTML
+    = date.toDateString();
 
-let days = "";
+    let days = "";
 
-for(let i = 1; i <= lastDay;i++){
-    if(i === today){
-        days += '<div class="today">'+i+'</div>';
+    for(let i = cmfirstDayIndex-1 ; i >= 0; i-- ){
+        let temp = pmlastDay - i;
+        days += '<div class="text-info">'+ temp +'</div>';
     }
-    else{
-        days += '<div>'+i+'</div>';
+
+    for(let i = 1; i <= cmlastDay;i++){
+        if(i === today){
+            days += '<div class="today">'+i+'</div>';
+        }
+        else{
+            days += '<div>'+i+'</div>';
+        }
     }
+
+    for(let i = cmlastDayIndex+1 ; i <= 6; i++ ){
+        let temp = i - cmlastDayIndex;
+        days += '<div class="text-info">'+ temp +'</div>';
+    }
+
+    monthDays.innerHTML = days;
 }
 
-monthDays.innerHTML = days;
+
+
+$('.pre').click(function(){
+    date.setMonth(date.getMonth()-1);
+    dateGenerate();
+});
+
+$('.next').click(function(){
+    date.setMonth(date.getMonth()+1);
+    dateGenerate();
+})
+
+dateGenerate();

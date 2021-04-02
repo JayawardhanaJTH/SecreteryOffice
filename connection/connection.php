55ef63307ac83e3b453b8275305778cebdbd163f
Jayawardana JTH <49198952+JayawardanaJTH@users.noreply.github.com>
2021-04-02 13:19:46 +0530

    set charset to UTF8

--- connection/connection.php
+++ connection/connection.php
@@ -6,6 +6,7 @@
     
     try {
             $conn = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE);
+            mysqli_set_charset($conn, 'utf8');
             
         if($conn->connect_error){
                 die ("Connection failed " .$conn->connect_error);
--- support/header.php
+++ support/header.php
@@ -7,7 +7,12 @@
 
 	<!-- Site title -->
 	<title>Divisional Secretary Office - Home</title>
-	<meta charset="utf-8">
+
+	<?php
+		header ('Content-Type: text/html; charset=utf-8');
+	?>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
 	<!-- Favicon-->

7a386befdd362cac9df2bfb625a83087cd8fbef0
JayawardanaJTH <tiran2323@gmail.com>
2022-11-13 00:54:36 +0530

    fixed UI alignment issues

--- layout/scripts/calender.js
+++ layout/scripts/calender.js
@@ -1,130 +1,143 @@
-let date = new Date()
+let date = new Date();
 var eventsDates = [];
 var eventsIDs = [];
 var counter = 0;
 
-function loadDates(dates,ids){
+function loadDates(dates, ids) {
+  for (let i = 0; i < dates.length; i++) {
+    eventsDates.push(dates[i]);
+  }
+  for (let i = 0; i < ids.length; i++) {
+    eventsIDs.push(ids[i]);
+  }
 
-    for(let i = 0;i<dates.length; i++){
-        eventsDates.push(dates[i]);
-    }
-    for(let i = 0;i<ids.length; i++){
-        eventsIDs.push(ids[i]);
-    }
-
-    dateGenerate();
+  dateGenerate();
 }
 
-let dateGenerate = function(){
-    const year = new Date(date.getFullYear(),date.getMonth()).getFullYear();
-    
-    //past month last day
-    const pmlastDay = new Date(date.getFullYear(),date.getMonth(),0).getDate();
+let dateGenerate = function () {
+  const year = new Date(date.getFullYear(), date.getMonth()).getFullYear();
 
-    //current month first day index
-    const cmfirstDayIndex = new Date(date.getFullYear(),date.getMonth(),1).getDay();
-    //current month last day index
-    const cmlastDayIndex = new Date(date.getFullYear(),date.getMonth()+1,0).getDay();
-    //current month last day 
-    const cmlastDay = new Date(date.getFullYear(),date.getMonth()+1,0).getDate();
+  //past month last day
+  const pmlastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
 
-    const today = date.getDate();
+  //current month first day index
+  const cmfirstDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    1
+  ).getDay();
+  //current month last day index
+  const cmlastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
+  //current month last day
+  const cmlastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
 
-    const months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"
-    ];
+  const today = date.getDate();
 
-    const monthDays = document.querySelector('.day');
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-    document.querySelector('.days h1').innerHTML
-    = months[date.getMonth()];
+  const monthDays = document.querySelector(".day");
 
-    document.querySelector('.days p').innerHTML
-    = date.toDateString();
+  document.querySelector(".days h1").innerHTML = months[date.getMonth()];
 
-    let days = "";
+  document.querySelector(".days p").innerHTML = date.toDateString();
 
-    for(let i = cmfirstDayIndex-1 ; i >= 0; i-- ){
-        let temp = pmlastDay - i;
-        days += '<div class="text-info">'+ temp +'</div>';
-    }
+  let days = "";
 
-    for(let i = 1; i <= cmlastDay;i++){
-        if(i === today){
-            let flag = false;
+  for (let i = cmfirstDayIndex - 1; i >= 0; i--) {
+    let temp = pmlastDay - i;
+    days += '<div class="text-info">' + temp + "</div>";
+  }
 
-            for(let j = 0;j<eventsDates['length']; j++){
-                if(i == eventsDates[j]  && counter == 0){
-                    flag = true;
-                    days += '<a href="php/event-add.php?view_id='+eventsIDs[j]+'" > <div class="today">'+i+'</div></a>';
-                }
-            }
-            if(!flag){
+  for (let i = 1; i <= cmlastDay; i++) {
+    if (i === today) {
+      let flag = false;
 
-                days += '<div class="today">'+i+'</div>';
-            }
+      for (let j = 0; j < eventsDates["length"]; j++) {
+        if (i == eventsDates[j] && counter == 0) {
+          flag = true;
+          days +=
+            '<a href="php/event-add.php?view_id=' +
+            eventsIDs[j] +
+            '" > <div class="today">' +
+            i +
+            "</div></a>";
         }
-        else{
-            let flag = false;
+      }
+      if (!flag) {
+        days += '<div class="today">' + i + "</div>";
+      }
+    } else {
+      let flag = false;
 
-            for(let j = 0;j<eventsDates['length']; j++){
-                if(i == eventsDates[j] && counter == 0){
-                    flag = true;
-                    days += '<a href="php/event-add.php?view_id='+eventsIDs[j]+'" > <div class="bg-info">'+i+'</div></a>';
-                }
-            }
-            if(!flag){
-                days += '<div>'+i+'</div>';
-            }
+      for (let j = 0; j < eventsDates["length"]; j++) {
+        if (i == eventsDates[j] && counter == 0) {
+          flag = true;
+          days +=
+            '<a class="bg-info" href="php/event-add.php?view_id=' +
+            eventsIDs[j] +
+            '" >' +
+            i +
+            "</a>";
         }
+      }
+      if (!flag) {
+        days += "<div>" + i + "</div>";
+      }
     }
+  }
 
-    if((cmfirstDayIndex == 5 && cmlastDay == 31) || (cmfirstDayIndex == 6 && cmlastDay == 31)){
-
-        for(let i = cmlastDayIndex+1 ; i <= 6; i++ ){
-            let temp = i - cmlastDayIndex;
-            days += '<div class="text-info">'+ temp +'</div>';
-        }
+  if (
+    (cmfirstDayIndex == 5 && cmlastDay == 31) ||
+    (cmfirstDayIndex == 6 && cmlastDay == 31)
+  ) {
+    for (let i = cmlastDayIndex + 1; i <= 6; i++) {
+      let temp = i - cmlastDayIndex;
+      days += '<div class="text-info">' + temp + "</div>";
     }
-    else if(cmfirstDayIndex == 6 && cmlastDay == 30){
-
-        for(let i = cmlastDayIndex+1 ; i <= 6; i++ ){
-            let temp = i - cmlastDayIndex;
-            days += '<div class="text-info">'+ temp +'</div>';
-        }
+  } else if (cmfirstDayIndex == 6 && cmlastDay == 30) {
+    for (let i = cmlastDayIndex + 1; i <= 6; i++) {
+      let temp = i - cmlastDayIndex;
+      days += '<div class="text-info">' + temp + "</div>";
     }
-    else{
-        for(let i = cmlastDayIndex+1 ; i <= 13; i++ ){
-            let temp = i - cmlastDayIndex;
-            days += '<div class="text-info">'+ temp +'</div>';
-        }
+  } else {
+    for (let i = cmlastDayIndex + 1; i <= 13; i++) {
+      let temp = i - cmlastDayIndex;
+      days += '<div class="text-info">' + temp + "</div>";
     }
+  }
 
-    monthDays.innerHTML = days;
-}
+  monthDays.innerHTML = days;
+};
 
-
-
-$('.pre').click(function(){
-    counter--;
-    date.setMonth(date.getMonth()-1);
-    dateGenerate();
+$(".pre").click(function () {
+  counter--;
+  date.setMonth(date.getMonth() - 1);
+  dateGenerate();
 });
 
-$('.next').click(function(){
-    counter++;
-    date.setMonth(date.getMonth()+1);
-    dateGenerate();
-})
-
+$(".next").click(function () {
+  counter++;
+  date.setMonth(date.getMonth() + 1);
+  dateGenerate();
+});
